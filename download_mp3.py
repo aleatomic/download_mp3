950fb664a71d8cc5498db92145ea89d7cba54a00
Alejandro Gomez <ale4t0mic@outlook.com>
2025-03-07 00:05:53 -0600

    fixDir
    Se agrega la creacion de la carpeta musica

--- download_mp3.py
+++ download_mp3.py
@@ -1,10 +1,17 @@
 # Importa la librería yt-dlp para descargar el audio de un video de YouTube en formato mp3
+import os
 import yt_dlp
+
+CARPETA_MUSICA = "musica" # Carpeta donde se guardarán los archivos mp3
+
+#Si no existe la carpeta, la creamos
+if not os.path.exists(CARPETA_MUSICA):
+    os.makedirs(CARPETA_MUSICA)
 
 def descargar_mp3(url):
     opciones = {
         'format': 'bestaudio/best', # Descarga el mejor audio disponible
-        'outtmpl': '%(title)s.%(ext)s', # Guarda el archivo con el título del video
+        'outtmpl': f'{CARPETA_MUSICA}/%(title)s.%(ext)s', # Guarda el archivo con el título del video
         #'optmpl': 'musica/%(title)s.%(ext)s',
         'postprocessors': [ # Convertimos el audio descargado a mp3
             {'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'},
